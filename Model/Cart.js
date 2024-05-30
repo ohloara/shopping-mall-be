9585135d7ff7db842c45cb5e590ebd483865332b
Udemy100 <srk03041@naver.com>
2024-05-31 00:36:54 +0900

    쇼핑몰 만들기 1~4

---
+++ Model/Cart.js
@@ -0,0 +1,23 @@
+const monggose = require('mongoose');
+const User = require("./User");
+const Product = require("./Product");
+const Schema = monggose.Schema;
+const cartSchema = Schema({
+    userId:{type:monggose.ObjectId, ref:User},
+    items:[{
+        productId:{type:monggose.ObjectId, ref:Product},
+        size:{type:String, required:true},
+        qty:{type:Number, default:1, required:true}
+    }]
+},{timestamps:true});
+
+cartSchema.methods.toJSON = function(){
+    const obj = this._doc;
+    delete obj.__v;
+    delete obj.updateAt;
+    delete obj.createAt;
+    return obj;
+}
+
+const Cart = monggose.model("Cart", cartSchema);
+module.exports = Cart;
---
+++ Model/Order.js
@@ -0,0 +1,30 @@
+const monggose = require("mongoose");
+const User = require("./User");
+const Product = require("./Product");
+const Schema = monggose.Schema;
+const orderSchema = Schema({
+    shipTo:{type:Object, required:true},
+    contact:{type:Object, required:true},
+    totalPrice:{type:Number, required:true, default:0},
+    userId:{type:monggose.ObjdectId, ref:User},
+    status:{type:String, default:"preparing"},
+    items:[{
+        productId:{type:monggose.ObjectId, ref:Product},
+        qty:{type:Number, required:true, default:1},
+        size:{type:String, required:true},
+        price:{type:Number, required:true}
+    }]
+
+},{timestamp:true});
+
+orderSchema.method.toJSON = function(){
+    const obj = this._doc;
+    delete obj.__v;
+    delete obj.updateAt;
+    delete obj.createAt;
+    return obj;
+};
+
+const Order = monggose.model("Order", orderSchema);
+
+module.exports = Order;
