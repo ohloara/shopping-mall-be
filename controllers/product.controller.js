000835e19b27068a077d6f53c584abedc3a718f7
Udemy100 <srk03041@naver.com>
2024-06-08 03:27:01 +0900

    쇼핑몰 만들기 18~19

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -17,9 +17,11 @@ productController.createProduct = async (req,res)=>{
 productController.getProducts = async(req, res)=>{
     try{
         const {page,name} = req.query;
-        const cond = name?{name:{$regex:name,$options:'i'}}:{};
+        let response = { status: "success" };
+        const cond = name
+            ? { name: { $regex: name, $options: "i" }, isDeleted: false }
+            : { isDeleted: false };
         let query = Product.find(cond);
-        let response = {status: "success"};
         if(page){
             query.skip((page-1)*PAGE_SIZE).limit(PAGE_SIZE);
             const totalItemNum = await Product.find(cond).count();
@@ -30,8 +32,49 @@ productController.getProducts = async(req, res)=>{
         response.data = productList;
         res.status(200).json(response);
     }catch(error){
-        res.status(400).json({status:"fail",error:error.message});
+        return res.status(400).json({status:"fail",error:error.message});
     }
 }
+
+productController.getProductById = async (req, res) => {
+    try{
+        const productId = req.params.id;
+        const product = await Product.findById(productId);
+        if (!product) throw new Error("No item found");
+        res.status(200).json({ status: "success", data: product });
+    }catch(error) {
+        return res.status(400).json({ status: "fail", error: error.message });
+    }
+};
+
+productController.updateProduct = async (req,res)=>{
+    try{
+        const productId = req.params.id;
+        const {sku,name,size,image,price,description,category,stock,status} = req.body;
+        const product = await Product.findByIdAndUpdate(
+            {_id:productId},
+            {sku,name,size,image,price,description,category,stock,status},
+            {new:true}
+        );
+        if(!product) throw new Error("item doesn't exist");
+        res.status(200).json({status:"success", data:product});
+    }catch(error){
+        return res.status(400).json({status:"fail",error:error.message});
+    }
+}
+
+productController.deleteProduct = async (req, res) => {
+    try{
+        const productId = req.params.id;
+        const product = await Product.findByIdAndUpdate(
+            { _id: productId },
+            { isDeleted: true }
+        );
+        if (!product) throw new Error("No item found");
+        res.status(200).json({ status: "success" });
+    }catch(error){
+        return res.status(400).json({ status: "fail", error: error.message });
+    }
+};
 
 module.exports = productController;
