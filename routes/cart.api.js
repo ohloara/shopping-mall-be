1f17f34ef4eef75318f1e1a0a2acbf4b859ca8b6
Udemy100 <srk03041@naver.com>
2024-06-09 04:11:20 +0900

    쇼핑몰 만들기 22~23

--- routes/cart.api.js
+++ routes/cart.api.js
@@ -4,5 +4,10 @@ const cartController = require("../controllers/cart.controller");
 const authController = require("../controllers/auth.controller");
 
 router.post('/', authController.authenticate, cartController.addItemToCart);
+router.get('/', authController.authenticate, cartController.getCart);
+router.delete("/:id", authController.authenticate, cartController.deleteCartItem);
+router.put("/:id", authController.authenticate, cartController.editCartItem);
+router.get("/qty", authController.authenticate, cartController.getCartQty);
+
 
 module.exports=router;
